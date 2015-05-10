020b08f4d0c20f7706c80eccf6eed48362b364ef
Nikhil Walvekar <nikhil.walvekar@synerzip.com>
2015-05-09 18:53:48 -0700

    Read static directory as argument.

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ var express = require('express');
 var app = express();
 var compress = require('compression');
 
-app.use("/", express.static(__dirname + '/public'));
-app.use(compress({
-  threshold: 100
-}));
+if (process.argv.length > 2) {
+	app.use("/", express.static(process.argv[2]));
+	app.use(compress({
+	  threshold: 100
+	}));
 
-var port = process.env.PORT || 8080;
-app.listen(port, function(){
-	console.log("Listening... on ", port);
-});
+	var port = process.env.PORT || 8080;
+	app.listen(port, function(){
+		console.log("Listening... on ", port);
+	});
+} else {
+	console.log("Please specify the static directory to serve.");
+}
